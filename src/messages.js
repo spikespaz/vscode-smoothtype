5069430c623bcad3e2c7376b7396c4aabd22f9fc
spikespaz <spikespaz@outlook.com>
2018-06-06 21:07:41 -0700

    Rename app.js to extension.js

--- src/messages.js
+++
@@ -1,9 +0,0 @@
-exports.messages = {
-    "needAdministrator": "Run VS Code with admin privileges so the changes can be applied.",
-    "enabled": "SmoothType enabled. Restart to take effect. If VSCode complains it is corrupted, click don't show again. See README for more detail.",
-    "disabled": "SmoothType reverted. Restart to take effect.",
-    "alreadyDisabled": "SmoothType is already disabled.",
-    "unknownError": "Something went wrong: ",
-    "restartIde": "Restart Visual Studio Code.",
-    "notConfigured": "Animation duration is not configured. Please set 'vscode_smoothtype.duration' in your user settings."
-};
