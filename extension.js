d5220210cc19178a479077f026cd82185cf6919a
spikespaz <spikespaz@outlook.com>
2018-06-08 04:59:13 -0700

    Rewrite enable and disable functions asynchronously

--- extension.js
+++ extension.js
@@ -4,6 +4,7 @@ const path = require("path");
 const tmp = require("tmp");
 const fs = require("fs");
 
+
 // Predefined to be used for errors and information.
 const messages = {
     enabled: "Smooth Typing has been enabled. Please restart the window.",
@@ -15,6 +16,7 @@ const messages = {
     disableFailed: "Failed to disable Smooth Typing.",
     reloadFailed: "Failed to reload Smooth Typing."
 };
+
 
 // Paths and directoies to VS Code itself.
 const appDirectory = path.dirname(require.main.filename);
@@ -33,37 +35,106 @@ const injectionTemplate =
 // Pattern to check if the code is injected.
 const injectionPattern = new RegExp("\\s*" + beginComment + "(?:.|\\s)*" + endComment);
 
+
 // Export the activate function to be called whenever the extension is initialized.
 exports.activate = activate;
 
 // Subscribe all of the command functions to the corresponding contributions.
 function activate(context) {
     console.info("Application Directory: ", appDirectory);
-    console.info("Index File Path: ", indexPath);
+    console.info("Application Index: ", indexPath);
 
     context.subscriptions.push(vscode.commands.registerCommand("extension.enableAnimation", enableAnimation));
     context.subscriptions.push(vscode.commands.registerCommand("extension.disableAnimation", disableAnimation));
     context.subscriptions.push(vscode.commands.registerCommand("extension.reloadAnimation", reloadAnimation));
 }
 
-// Writes text to a file, after requesting elevated permissions, and return success.
-function writeFileAdmin(filePath, writeString) {
-    let tempFilePath = tmp.fileSync().name;
 
-    fs.writeFileSync(tempFilePath, writeString, "utf-8");
+function enableAnimation() {
+    let config = vscode.workspace.getConfiguration("smoothtype");
 
-    sudo.exec(process.platform === "win32" ? "copy " : "cp " +
-        tempFilePath + " " + filePath, { name: "Visual Studio Code" }
-    );
-
-    if (fs.readFileSync(filePath, "utf-8") === writeString)
-        return true;
-    else return false;
+    injectCursorStyle(config.duration).then(() => {
+        if (config.autoReload) reloadWindow();
+        else reloadWindow(messages.enabled);
+    }, (reason) => {
+        vscode.window.showWarningMessage(messages.enableFailed);
+        console.error(messages.enableFailed, "\n", reason);
+    });
 }
 
-// Function to reload the window, asks and displays a message if "message" is provided.
-function reloadWindow(message = null) {
-    if (message === null) {
+
+function disableAnimation() {
+    let config = vscode.workspace.getConfiguration("smoothtype");
+
+    removeCursorStyle().then(() => {
+        if (config.autoReload) reloadWindow();
+        else reloadWindow(messages.disabled);
+    }, (reason) => {
+        vscode.window.showWarningMessage(messages.disableFailed);
+        console.error(messages.disableFailed, "\n", reason);
+    });
+}
+
+
+function reloadAnimation() { }
+
+
+function injectCursorStyle(duration) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(indexPath, "utf-8", (error, html) => {
+            if (error) reject(error);
+            else {
+                html = html.replace("</head>",
+                    injectionTemplate.replace("{duration}", duration) + "\n\t</head>");
+
+                writeFileAdmin(
+                    indexPath, html, "utf-8", "Visual Studio Code"
+                ).then(resolve, reject);
+            }
+        });
+    });
+}
+
+
+function removeCursorStyle() {
+    return new Promise((resolve, reject) => {
+        fs.readFile(indexPath, "utf-8", (error, html) => {
+            if (error) reject(error);
+            else {
+                html = html.replace(injectionPattern, "");
+
+                writeFileAdmin(
+                    indexPath, html, "utf-8", "Visual Studio Code"
+                ).then(resolve, reject);
+            }
+        });
+    });
+}
+
+
+function writeFileAdmin(filePath, writeString, encoding = "utf-8", promptName = "File Writer") {
+    return new Promise((resolve, reject) => {
+        tmp.file((error, tempFilePath) => {
+            if (error) reject(error);
+            else fs.writeFile(tempFilePath, writeString, encoding, (error) => {
+                if (error) reject(error);
+                else sudo.exec(
+                    (process.platform === "win32" ? "copy " : "cp ") +
+                    "\"" + tempFilePath + "\" \"" + filePath + "\"",
+                    { name: promptName },
+                    (error) => {
+                        if (error) reject(error);
+                        else resolve(error);
+                    }
+                );
+            });
+        });
+    });
+}
+
+
+function reloadWindow(message) {
+    if (message === undefined) {
         console.info("Reloading window.");
 
         vscode.commands.executeCommand("workbench.action.reloadWindow");
@@ -73,106 +144,7 @@ function reloadWindow(message = null) {
         vscode.window.showInformationMessage(message, {
             title: "Reload Window"
         }).then(clicked => {
-            if (clicked !== undefined) reloadWindow();
+            if (clicked) reloadWindow();
         });
     }
-}
-
-// Injects the template string into the VS Code index file, and returns true if successful.
-function injectCursorStyle(duration) {
-    console.info("Injecting Smooth Typing style.");
-
-    let indexHTML = fs.readFileSync(indexPath, "utf-8");
-    indexHTML = indexHTML.replace("</head>",
-        injectionTemplate.replace("{duration}", duration) + "\n\t</head>"
-    );
-
-    return writeFileAdmin(indexPath, indexHTML);
-}
-
-// Uses "injectionPattern" to remove the injected template from the index file.
-function removeCursorStyle() {
-    console.info("Removing Smooth Typing style.");
-
-    let indexHTML = fs.readFileSync(indexPath, "utf-8");
-    indexHTML = indexHTML.replace(injectionPattern, "");
-
-    return writeFileAdmin(indexPath, indexHTML);
-}
-
-// Checks if the "injectionPattern" is present in the index file.
-function checkInjection() {
-    console.info("Checking if Smooth Typing styles are already injected.");
-
-    let indexHTML = fs.readFileSync(indexPath, "utf-8");
-    return injectionPattern.test(indexHTML);
-}
-
-// Command to enable Smooth Typing by injecting code into the index file.
-function enableAnimation() {
-    console.info("Enabling Smooth Typing animation.");
-
-    if (checkInjection()) {
-        console.warn(messages.alreadyEnabled);
-
-        vscode.window.showWarningMessage(messages.alreadyEnabled);
-        return;
-    }
-
-    let config = vscode.workspace.getConfiguration("smoothtype");
-
-    if (injectCursorStyle()) {
-        console.info(messages.enabled);
-        reloadWindow(config.autoReload ? null : messages.enabled);
-    } else {
-        console.error(messages.enableFailed);
-        vscode.window.showErrorMessage(messages.enableFailed);
-    }
-}
-
-// Command to reverse "enableAnimation".
-function disableAnimation() {
-    console.info("Disabling Smooth Typing animation.");
-
-    if (!checkInjection()) {
-        console.warn(messages.alreadyDisabled);
-
-        vscode.window.showWarningMessage(messages.alreadyDisabled);
-        return;
-    }
-
-    let config = vscode.workspace.getConfiguration("smoothtype");
-
-    if (removeCursorStyle()) {
-        console.info(messages.disabled);
-        reloadWindow(config.autoReload ? null : messages.disabled);
-    } else {
-        console.error(messages.disableFailed);
-        vscode.window.showErrorMessage(messages.disableFailed);
-    }
-}
-
-// Disable and re-enable Smooth Typing, useful for after VS Code updates.
-function reloadAnimation() {
-    console.info("Reloading Smooth Typing animation.");
-
-    let config = vscode.workspace.getConfiguration("smoothtype");
-
-    if (checkInjection())
-        if (!removeCursorStyle()) {
-            console.error(messages.reloadFailed);
-            vscode.window.showErrorMessage(messages.reloadFailed);
-
-            return;
-        }
-
-
-    if (injectCursorStyle(config.duration)) {
-        console.info(messages.reloaded);
-        reloadWindow(config.autoReload ? null : messages.reloaded);
-    } else {
-        console.error(messages.reloadFailed);
-        vscode.window.showErrorMessage(messages.reloadFailed);
-    }
-
 }
